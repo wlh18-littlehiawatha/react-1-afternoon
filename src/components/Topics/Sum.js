811009e37b59dbbaf993c92973b441c2c0c1d8f4
Jacob Hall <halljaco@yahoo.com>
2020-03-26 19:50:36 -0600

    solution code commented out for additional refference

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -55,3 +55,143 @@ class Sum extends Component {
 }
 
 export default Sum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ---------------------> Solution Code <------------------------
+
+// import React, { Component } from 'react';
+
+// export default class Sum extends Component {
+
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       number1: 0,
+//       number2: 0,
+//       sum: null
+//     }
+//   }
+
+//   updateNumber1(val) {
+//     this.setState({ number1: parseInt(val, 10) });
+//   }
+
+//   updateNumber2(val) {
+//     this.setState({ number2: parseInt(val, 10) });
+//   }
+
+//   add(num1, num2) {
+//     this.setState({ sum: num1 + num2 });
+//   }
+
+//   render() {
+//     return (
+//       <div className="puzzleBox sumPB">
+//         <h4> Sum </h4>
+//         <input className="inputLine" type="number" onChange={ (e) => this.updateNumber1(e.target.value) }/>
+//         <input className="inputLine" type="number" onChange={ (e) => this.updateNumber2(e.target.value) }/>
+//         <button className="confirmationButton" onClick={ () => this.add(this.state.number1, this.state.number2) }> Add </button>
+//         <span className="resultsBox"> Sum: {this.state.sum} </span>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ----------------------> After Hours Solution Code <------------------- 
+
+
+
+// import React, { Component } from 'react';
+
+// export default class Sum extends Component {
+//   constructor(){
+//     super()
+
+//     this.state = {
+//       number1: 0,
+//       number2: 0,
+//       sum: null
+//     }
+//   }
+
+//   handleInputOne(event){
+//     this.setState({
+//       number1: +event.target.value
+//     })
+//   }
+
+//   handleInputTwo(event){
+//     this.setState({
+//       number2: +event.target.value
+//     })
+//   }
+
+//   // handleClick(){
+
+//   // }
+
+//   handleClick = () => {
+//     // This method should update the value of sum.
+//     // This method should solve the toy problem.
+
+//     this.setState({
+//       sum: this.state.number1 + this.state.number2
+//     })
+//   }
+
+//   render() {
+//     console.log(typeof this.state.number1)
+//     console.log(typeof this.state.number2)
+//     console.log(typeof this.state.sum)
+//     return (
+//       <div className="puzzleBox sumPB">
+//         <h4>Sum</h4>
+//         <input className="inputLine" onChange={(event) => this.handleInputOne(event)} />
+//         <input className="inputLine" onChange={(event) => this.handleInputTwo(event)} />
+//         <button className="confirmationButton" onClick={this.handleClick} >Click Me!</button>
+//     <span className="resultsBox">Sum: {this.state.sum}</span>
+//       </div>
+//     )
+//   }
+// }
